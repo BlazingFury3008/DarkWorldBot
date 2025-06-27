c08eff1a6c511832610058c00f51546c84172ae7
Aiden Taylor <ataylor3008@gmail.com>
2025-06-27 10:08:53 +0100

    Start of bot

---
+++ bot/bot.py
@@ -0,0 +1,17 @@
+import discord
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on As {self.user}')
+    
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+        
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run(config.DISCORD_KEY)
